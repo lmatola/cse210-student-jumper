f5e7496dab93a0c33cf88c7f7eab9015b64c0d58
awarbler <awoodfor@byui.edu>
2021-10-16 08:36:32 -0500

    updated puzzle parachute

--- jumper/game/puzzle_parachute.py
+++ jumper/game/puzzle_parachute.py
@@ -2,30 +2,30 @@
 class Puzzle_parachute:
 
     def __init__(self):
-      pass
+      self.draw_parachute()
 
     '''draw_parachute just makes the parachute and holds its value in a string whether is complete or gone'''
 
- #def draw_parachute():    
+ 
     parachute = [" ___","/___\ ", "\   /", " \ /"]
     jumper = ["  0 "," /|\ ", " / \ "," ","^^^^^^^"]
 
-
-    for x in range(len(parachute)):
-        print (parachute[x])
-    for x in range(len(jumper)):
-        print (jumper[x])
+#def draw_parachute(self):    
+    for part in range(len(parachute)):
+        print (parachute[part])
+    for part in range(len(jumper)):
+        print (jumper[part])
 
 
     '''remove_parachute_piece is only called if the input of correct guess is false, 
     it removes a part of the parachute  like in the pictures of the game.'''
-    def remove_parachute_piece(parachute,jumper):
-        del parachute[0]
+    def remove_parachute_piece(self):
+        del self.parachute[0]
 
     '''keep_playing is dependent on if the parachute is all gone and the head of the jumper 
     is "x" instead of "o" like the picture. If the person died (''x") then keep playing is f
     alse. Otherwise it is true.'''
-def keep_playing(jumper):
+def keep_playing(self,jumper):
     if jumper[0] != "x":
          True
 
